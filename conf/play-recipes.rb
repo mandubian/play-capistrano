1012d118e2002a7989026941074f051fc59cedaf
Yamashita Yuu <yamashita@geishatokyo.com>
2012-02-03 19:09:23 +0900

    added play:setup task to download & install play via HTTP

--- conf/play-recipes.rb
+++ conf/play-recipes.rb
@@ -43,6 +43,47 @@ namespace :deploy do
 end
 
 namespace :play do
+  _cset :play_version, '1.2.4'
+  _cset :play_zip_url do
+    "http://download.playframework.org/releases/#{File.basename(play_zip_file)}"
+  end
+  _cset :play_zip_file do
+    File.join('/tmp', "play-#{play_version}.zip")
+  end
+  _cset :play_path do
+#   File.join(shared_path, "play-#{play_version}")
+    abort "you must specify install path for play"
+  end
+  _cset :play_cmd do
+    File.join(play_path, 'play')
+  end
+  _cset :play_preserve_zip, true
+
+  namespace :setup do
+    desc "install play if needed"
+    task :default do
+      transaction {
+        install_play
+      }
+    end
+
+    task :install_play do
+      on_rollback {
+        files = [ play_path ]
+        files << play_zip_file unless play_preserve_zip
+        run "rm -rf #{files.join(' ')}"
+      }
+      run "rm -f #{play_zip_file}" unless play_preserve_zip
+      run <<-E
+        if ! test -d #{play_path}; then
+          (test -f #{play_zip_file} || wget --no-verbose -o #{play_zip_file} #{play_zip_url}; true) &&
+          unzip #{play_zip_file} -d #{File.dirname(play_path)} && test -x #{play_path}/play;
+        fi
+      E
+      run "rm -f #{play_zip_file}" unless play_preserve_zip
+    end
+  end
+
   desc "view play pid"
   task :pid do
     run "cd #{app_path}; #{play_path}/play pid --pid_file=#{app_pid}"
